f273df851c3bfdfa808ff3005ef92ccb8699fa52
mayank0109 <mayankkhanna038@gmail.com>
2023-03-06 20:18:13 +0530

    More fixed

--- app/controllers/slack_notifications_controller.rb
+++ app/controllers/slack_notifications_controller.rb
@@ -5,6 +5,8 @@ class SlackNotificationsController < ApplicationController
     if params[:Type] == "SpamNotification"
       Slack::Notifier.new(params).notify
       render json: { notice: "slack notification successfully" }, status: :ok
+    else
+      render json: { message: "not a spam" }, status: :ok
     end
   rescue StandardError => ex
     render json: { errors: ex.message }, status: :unprocessable_entity
